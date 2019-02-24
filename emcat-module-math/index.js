74ad3d8d2b4c0fa72fca3c3f23a47f0013460c88
RGFTheCoder <recdeclec.ceo@gmail.com>
2019-02-24 02:54:44 -0500

    add Math module

---
+++ emcat-module-math/index.js
@@ -0,0 +1,14 @@
+module.exports.sin = function sin(x) {
+    let iterNum = 2 ** 3;
+    let mxx = -(x ** 2);
+    let sin = 1;
+    let n = 0;
+    let term = 1;
+    for (let i = 1; i <= 2 * iterNum; i++) {
+        n = n + 2;
+        term = term * mxx / (n * (n + 1));
+        sin = sin + term
+    }
+    sin = x * sin;
+    return sin;
+}
