3ca6980a948dac6d2a2fa3d9a61d8b3b36619664
RGFTheCoder <recdeclec.ceo@gmail.com>
2019-02-24 03:03:30 -0500

    add cos

--- emcat-module-math/index.js
+++ emcat-module-math/index.js
@@ -12,3 +12,18 @@ module.exports.sin = function sin(x) {
     sin = x * sin;
     return sin;
 }
+
+module.exports.cos = function cos(x) {
+    let iterNum = 2 ** 3;
+    let mxx = -(x ** 2);
+    let sin = 1;
+    let n = 1;
+    let term = 1;
+    for (let i = 1; i <= 2 * iterNum; i++) {
+        n = n + 2;
+        term = term * mxx / (n * (n + 1));
+        sin = sin + term
+    }
+    sin = x * sin;
+    return sin;
+}
